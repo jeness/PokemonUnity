639a65f2980b61ecdaf39ff11b1f0f42ed96e2ee
jeness <yuhaoran9968@gmail.com>
2018-03-09 03:09:24 -0500

    first commit on pokemon unity project

---
+++ NewARproject/Assets/Scripts/Button2_click.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button2_click : MonoBehaviour {
+
+    public Button mButton;
+
+    // Use this for initialization
+    void Start()
+    {
+        Button btn = mButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+    void TaskOnClick()
+    {
+        Application.LoadLevel(2);
+    }
+}
---
+++ NewARproject/Assets/Scripts/ButtonBack_click.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonBack_click : MonoBehaviour
+{
+
+    public Button mButton;
+
+    // Use this for initialization
+    void Start()
+    {
+        Button btn = mButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+    void TaskOnClick()
+    {
+        Application.LoadLevel(0);
+    }
+
+}
